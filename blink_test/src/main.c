551c5f6768b1c03b2600b74d75c1a16e01d1bd3a
Nathan Givens <nate.givens03@gmail.com>
2019-11-07 22:40:06 -0500

    feat: blink program for remote prog/debug

---
+++ blink_test/src/main.c
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f4xx.h"
+			
+
+int main(void)
+{
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  GPIOA->MODER &= ~(GPIO_MODER_MODER5);
+  GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+  for(;;) {
+    GPIOA->BSRRL = 1<<5;
+    for(int i=0;i<1000000;) {
+      i += 1;
+    }
+    GPIOA->BSRRH = 1<<5;
+    for(int i=0;i<1000000;) {
+      i += 1;
+    }
+  }
+}
